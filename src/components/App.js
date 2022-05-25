103b7a1400533b7b6164d550343eeb0669d05d53
Naftali Kulik <nkulik1994@gmail.com>
2022-05-25 17:01:52 -0400

    created Header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+import Header from './Header.js';
 import video from "../data/video.js";
 
 function App() {
@@ -8,11 +10,12 @@ function App() {
       <iframe
         width="919"
         height="525"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+        src={video.embedUrl}
         frameBorder="0"
         allowFullScreen
         title="Thinking in React"
       />
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <h1>React Today and Tomorrow and 90% Cleaner React With Hooks</h1>
+        </header>
+    )
+}
+
+export default Header
