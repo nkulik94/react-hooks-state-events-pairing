cfbe02abcf58c3b466605cc7e956b80f6373f664
Naftali Kulik <nkulik1994@gmail.com>
2022-05-25 17:32:39 -0400

    programmed vote buttons

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,12 @@ function App() {
 
   const [videoObj, updateVid] = useState(video)
 
+  function handleVotes(updatedKey, value) {
+    const newObj = {...videoObj}
+    newObj[updatedKey] = value
+    updateVid(newObj)
+  }
+
   return (
     <div className="App">
       <iframe
@@ -19,7 +25,7 @@ function App() {
         title="Thinking in React"
       />
       <Header views={videoObj.views} date={videoObj.createdAt} />
-      <VoteButtons upvotes={videoObj.upvotes} downvotes={videoObj.downvotes} />
+      <VoteButtons upvotes={videoObj.upvotes} downvotes={videoObj.downvotes} onClickCallback={handleVotes} />
     </div>
   );
 }
--- src/components/VoteButtons.js
+++ src/components/VoteButtons.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
-function VoteButtons({ upvotes, downvotes }) {
+function VoteButtons({ upvotes, downvotes, onClickCallback }) {
+
+    function voteCallback(e) {
+        e.target.id === 'upvotes' ? onClickCallback('upvotes', (upvotes + 1)) : onClickCallback('downvotes', (downvotes + 1))
+    }
+
 
     return (
         <div id='vote-buttons'>
-            <button>{upvotes}👍</button>
-            <button>{downvotes}👎</button>
+            <button id='upvotes' onClick={voteCallback}>{upvotes}👍</button>
+            <button id='downvotes'onClick={voteCallback}>{downvotes}👎</button>
         </div>
     )
 }
